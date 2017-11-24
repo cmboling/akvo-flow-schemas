c5252b2d6af4a9a486dfe1ff674db41043d48b2e
Dan Lebrero <dlebrero@gmail.com>
2017-11-24 17:43:14 +0100

    releasing every commit in master/develop with a unique version

--- ci/build.sh
+++ ci/build.sh
@@ -8,5 +8,16 @@ if [ -z "$TRAVIS_COMMIT" ]; then
     export TRAVIS_COMMIT=local
 fi
 
-docker pull maven:3.5.2-jdk-8-alpine
-docker run -v `pwd`:/app -v $HOME/.m2:/root/.m2 -e CLOJARS_PASSWORD="$CLOJARS_PASSWORD" -w /app maven:3.5.2-jdk-8-alpine mvn deploy -s /app/maven-ci-settings.xml
+if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+    exit 0
+fi
+
+if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
+    exit 0
+fi
+
+docker build --rm=false -t akvo-flow-schemas:develop . -f Dockerfile-ci
+docker run \
+    -v `pwd`:/app -v $HOME/.m2:/root/.m2 \
+    -e CLOJARS_PASSWORD="$CLOJARS_PASSWORD" -e TRAVIS_COMMIT="${TRAVIS_COMMIT}" -e TRAVIS_BUILD="${TRAVIS_BUILD}" \
+     akvo-flow-schemas:develop /script.sh
---
+++ ci/script.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -eu
+
+POM_VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | sed -n -e '/^\[.*\]/ !{ /^[0-9]/ { p; q } }'`
+
+RELEASE_VERSION=`echo ${POM_VERSION} | sed "s/-SNAPSHOT/.${TRAVIS_BUILD}.${TRAVIS_COMMIT}/"`
+
+echo "POM version: ${POM_VERSION}, Release version: ${RELEASE_VERSION}"
+
+mvn versions:set -DnewVersion=${RELEASE_VERSION}
+mvn deploy -s /app/maven-ci-settings.xml
