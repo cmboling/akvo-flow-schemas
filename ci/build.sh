321a7e986c58aba083f7efe8a1ae32637dbe3275
Dan Lebrero <dlebrero@gmail.com>
2017-11-24 18:00:18 +0100

    renaming "script.sh" to "release.sh"

--- ci/build.sh
+++ ci/build.sh
@@ -8,16 +8,17 @@ if [ -z "$TRAVIS_COMMIT" ]; then
     export TRAVIS_COMMIT=local
 fi
 
-if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
-    exit 0
-fi
-
 if [[ "${TRAVIS_PULL_REQUEST}" != "false" ]]; then
     exit 0
 fi
 
 docker build --rm=false -t akvo-flow-schemas:develop . -f Dockerfile-ci
+
+if [[ "${TRAVIS_BRANCH}" != "develop" ]] && [[ "${TRAVIS_BRANCH}" != "master" ]]; then
+    exit 0
+fi
+
 docker run \
     -v `pwd`:/app -v $HOME/.m2:/root/.m2 \
     -e CLOJARS_PASSWORD="$CLOJARS_PASSWORD" -e TRAVIS_COMMIT="${TRAVIS_COMMIT}" -e TRAVIS_BUILD_NUMBER="${TRAVIS_BUILD_NUMBER}" \
-     akvo-flow-schemas:develop /script.sh
+     akvo-flow-schemas:develop /release.sh
